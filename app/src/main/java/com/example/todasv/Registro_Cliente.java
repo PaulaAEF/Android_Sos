8e1124f7269742bc8633cc43a7d4c8614f13abb2
paulaoviedo <35734271+paulaoviedo@users.noreply.github.com>
2019-03-28 00:47:17 -0500

    Libreria Volley + Registro Volley

--- app/src/main/java/com/example/todasv/Registro_Cliente.java
+++ app/src/main/java/com/example/todasv/Registro_Cliente.java
@@ -1,22 +1,33 @@
 package com.example.todasv;
-
-import android.content.DialogInterface;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.method.Touch;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-public class Registro_Cliente extends AppCompatActivity {
-Button  registrar;
-EditText nombre, apellido,cc,fecha,tel,correo,pass;
+import org.json.JSONObject;
+
+
+public class Registro_Cliente extends AppCompatActivity implements Response.Listener<JSONObject>,Response.ErrorListener{
+    Button  registrar;
+    EditText nombre, apellido,cc,fecha,tel,correo,pass;
     String server_URL="HTTP://ippp /archivo.php";
     AlertDialog.Builder builder;
+    RequestQueue queue ;
+
+    JsonObjectRequest JsonObjectRequest;
+    ProgressDialog progress ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +42,32 @@ EditText nombre, apellido,cc,fecha,tel,correo,pass;
         correo= (EditText)findViewById(R.id.correo);
         pass= (EditText)findViewById(R.id.pass);
 
+        queue = Volley.newRequestQueue(getApplicationContext());
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CargarWebService();
+            }
+        });
+
+    }
+
+    private void CargarWebService() {
+        progress = new ProgressDialog(getApplicationContext());
+        progress.setMessage("Cargando....");
+        progress.show();
+
+        //localhost se cambia por la ip del equipo
+        String URL= "nombre="+nombre.getText().toString()+"apellido="+apellido.getText().toString()+"cedula="+cc.getText().toString()+"fechaExpedicion="+fecha.getText().toString()
+                +"telefono="+tel.getText().toString()+"correo="+correo.getText().toString()+"clave="+pass.getText().toString();
+        URL=URL.replace(" ","%20");
+        JsonObjectRequest= new JsonObjectRequest(Request.Method.GET,URL,null,this,this);
+        queue.add(JsonObjectRequest);
+
 
     }
 
     public void pasarTipAr(View view){
-
 
         String t1= nombre.getText().toString();
         String t2= apellido.getText().toString();
@@ -46,42 +78,59 @@ EditText nombre, apellido,cc,fecha,tel,correo,pass;
         String t7= pass.getText().toString();
 
 
-
-        try {
-
+        int n;
+        int a  = t7.length();
+        try{
             if (t1.matches("") || t2.matches("") || t3.matches("") || t4.matches("") || t5.matches("") || t6.matches("") || t7.matches("")) {
+
                 Toast.makeText(getApplicationContext(), "EL CAMPO ESTA VACIO", Toast.LENGTH_SHORT).show();
+            }else {
+                if (a>=6){
 
+                    a=t5.length();
+                    if (a==10){
+                        Intent pasarta = new Intent(this, tipo_de_arreglo.class);
+                        startActivity(pasarta);
+                    }else{
+                        Toast.makeText(getApplicationContext(), "VERIFIQUE SU NUMERO DE TELEFONO", Toast.LENGTH_SHORT).show();
+                    }
 
-            }else{
-                Intent pasarta = new Intent(this, tipo_de_arreglo.class);
+                }else{
 
-                startActivity(pasarta);
+                    Toast.makeText(getApplicationContext(), "LA CONTRASEÑA DEBE CONTENER MAS DE 6 CARACTERES", Toast.LENGTH_SHORT).show();
                 }
-            }catch(Exception e){
 
             }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        }catch (Exception e){
 
 
 
 
         }
 
+    }
 
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progress.hide();
+        Toast.makeText(getApplicationContext(),"Ocurrio un error y no puedo ser Registrado  "+error.toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
+        Toast.makeText(getApplicationContext(),"Se ha registro exitosamente",Toast.LENGTH_SHORT).show();
+        progress.hide();
+        nombre.setText("");
+        apellido.setText("");
+        fecha.setText("");
+        cc.setText("");
+        tel.setText("");
+        correo.setText("");
+        pass.setText("");
 
 
     }
-
+}
